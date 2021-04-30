98188793a59c67c9b8a3e058f929f4ec91368e7f
Gobikumaar-Sivagnanam <gobikumaar16@gmail.com>
2021-04-30 17:40:11 +0530

    Fixing Comments

--- src/temperature_set.c
+++ src/temperature_set.c
@@ -13,7 +13,7 @@
 void initialize_ADC(uint8_t channel)
 {
     ADMUX = (1<<REFS0);                                                         /// Setting AVcc to Aref
-    ADCSRA = (1<<ADEN) | (1<<ADPS0) | (1<<ADPS1) | (1<<ADPS2) | (1<<ADIE);      /// Since we have 8 channels 
+    ADCSRA = (1<<ADEN) | (1<<ADIE) | (1<<ADPS0) | (1<<ADPS1) | (1<<ADPS2);      /// Setting ADC Enable, Interrupt Enable, Prescaler 
     
     ADMUX &= 0xF8;                                                              /// Logic to set the correct channel based on input
     channel &= 0b00000111;
